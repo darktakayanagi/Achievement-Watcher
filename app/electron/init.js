09cdb10a0170336b41e55373d290252ff21cccc0
DarkTakayanagi <fabiolouro_96@hotmail.com>
2025-05-06 02:11:20 +0100

    handle main/overlay window creation

--- app/electron/init.js
+++ app/electron/init.js
@@ -2,13 +2,158 @@
 
 const { app, BrowserWindow, dialog, session, shell, ipcMain, globalShortcut } = require('electron');
 const remote = require('@electron/remote/main');
-
+remote.initialize();
 const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 const ipc = require(path.join(__dirname, 'ipc.js'));
 
+const manifest = require('../package.json');
+
+if (manifest.config['disable-gpu']) app.disableHardwareAcceleration();
+if (manifest.config.appid) app.setAppUserModelId(manifest.config.appid);
+let options = manifest.config.window;
+options.show = false;
+options.webPreferences = {
+  devTools: manifest.config.debug || false,
+  nodeIntegration: true,
+  contextIsolation: false,
+  webviewTag: false,
+  v8CacheOptions: manifest.config.debug ? 'none' : 'code',
+  enableRemoteModule: true,
+};
+
+let MainWin = null;
 let overlayWindow = null;
+
+function createMainWindow() {
+  if (MainWin) {
+    if (MainWin.isMinimized()) MainWin.restore();
+    MainWin.focus();
+    return;
+  }
+
+  //electron 9 crash if no icon exists to specified path
+  try {
+    fs.accessSync(options.icon, fs.constants.F_OK);
+  } catch {
+    delete options.icon;
+  }
+
+  MainWin = new BrowserWindow(options);
+
+  //Frameless
+  if (options.frame === false) MainWin.isFrameless = true;
+
+  //Debug tool
+  if (manifest.config.debug) {
+    MainWin.webContents.openDevTools({ mode: 'undocked' });
+    MainWin.isDev = true;
+    console.info((({ node, electron, chrome }) => ({ node, electron, chrome }))(process.versions));
+    try {
+      const contextMenu = require('electron-context-menu')({
+        append: (defaultActions, params, browserWindow) => [
+          {
+            label: 'Reload',
+            visible: params,
+            click: () => {
+              MainWin.reload();
+            },
+          },
+        ],
+      });
+    } catch (err) {
+      dialog.showMessageBoxSync({
+        type: 'warning',
+        title: 'Context Menu',
+        message: 'Failed to initialize context menu.',
+        detail: `${err}`,
+      });
+    }
+  }
+
+  //User agent
+  MainWin.webContents.userAgent = manifest.config['user-agent'];
+  session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+    details.requestHeaders['User-Agent'] = manifest.config['user-agent'];
+    callback({ cancel: false, requestHeaders: details.requestHeaders });
+  });
+
+  //External open links
+  const openExternal = function (event, url) {
+    if (!url.startsWith('file:///')) {
+      event.preventDefault();
+      shell.openExternal(url).catch(() => {});
+    }
+  };
+  MainWin.webContents.on('will-navigate', openExternal); //a href
+  MainWin.webContents.on('new-window', openExternal); //a href target="_blank"
+
+  //enable ipc
+  ipc.window(MainWin);
+  remote.initialize();
+
+  MainWin.loadFile(manifest.config.window.view);
+
+  const isReady = [
+    new Promise(function (resolve) {
+      MainWin.once('ready-to-show', () => {
+        return resolve();
+      }); //Window is loaded and ready to be drawn
+    }),
+    new Promise(function (resolve) {
+      ipcMain.handleOnce('components-loaded', () => {
+        return resolve();
+      }); //Wait for custom event
+    }),
+  ];
+
+  Promise.all(isReady).then(() => {
+    MainWin.show();
+    MainWin.focus();
+
+    setInterval(() => {
+      const command = `powershell -NoProfile -Command "Get-Process | Where-Object { $_.Path -ne $null } | ForEach-Object { try { $desc = (Get-Item $_.Path).VersionInfo.FileDescription } catch { $desc = 'N/A' }; $memoryUsage = $_.WorkingSet / 1MB; Write-Output ('{0}|{1}|{2}|{3}' -f $_.Name, $_.Id, $desc, $memoryUsage) }"`;
+      let found = false;
+      exec(command, (error, stdout) => {
+        if (!error) {
+          const lines = stdout.trim().split('\r\n');
+          for (const line of lines) {
+            const [name, pid, description, memory] = line.trim().split('|');
+            if (name.toLowerCase() === 'node' && description.toLowerCase().includes('achievement watcher')) {
+              found = true;
+            }
+          }
+        }
+        MainWin.webContents.send('watchdog-status', found);
+      });
+    }, 5000);
+
+    //overlay here to debug
+    globalShortcut.register('Control+Shift+O', () => {
+      if (overlayWindow && !overlayWindow.isDestroyed()) {
+        overlayWindow.close();
+        overlayWindow = null;
+      } else {
+        if (!selectedConfig) {
+          if (mainWindow && !mainWindow.isDestroyed()) {
+            mainWindow.webContents.send('notify', {
+              message: '⚠️ Select first a config!',
+              color: '#ff9800',
+            });
+          }
+          return;
+        }
+
+        createOverlayWindow(selectedConfig);
+      }
+    });
+  });
+
+  MainWin.on('closed', () => {
+    MainWin = null;
+  });
+}
 function createOverlayWindow(selectedConfig) {
   const { width, height } = require('electron').screen.getPrimaryDisplay().workAreaSize;
 
@@ -26,10 +171,44 @@ function createOverlayWindow(selectedConfig) {
     hasShadow: false,
     fullscreenable: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, '../overlayPreload.js'),
       contextIsolation: true,
       nodeIntegration: false,
+      devTools: manifest.config.debug || false,
     },
+  });
+
+  if (manifest.config.debug) {
+    overlayWindow.webContents.openDevTools({ mode: 'undocked' });
+    overlayWindow.isDev = true;
+    console.info((({ node, electron, chrome }) => ({ node, electron, chrome }))(process.versions));
+    try {
+      const contextMenu = require('electron-context-menu')({
+        append: (defaultActions, params, browserWindow) => [
+          {
+            label: 'Reload',
+            visible: params,
+            click: () => {
+              overlayWindow.reload();
+            },
+          },
+        ],
+      });
+    } catch (err) {
+      dialog.showMessageBoxSync({
+        type: 'warning',
+        title: 'Context Menu',
+        message: 'Failed to initialize context menu.',
+        detail: `${err}`,
+      });
+    }
+  }
+
+  //User agent
+  overlayWindow.webContents.userAgent = manifest.config['user-agent'];
+  session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+    details.requestHeaders['User-Agent'] = manifest.config['user-agent'];
+    callback({ cancel: false, requestHeaders: details.requestHeaders });
   });
 
   overlayWindow.setAlwaysOnTop(true, 'screen-saver');
@@ -38,18 +217,28 @@ function createOverlayWindow(selectedConfig) {
   overlayWindow.setFocusable(false);
   overlayWindow.blur();
 
-  overlayWindow.loadFile('../view/overlay.html');
-
+  overlayWindow.loadFile(path.join(__dirname, '..\\view\\overlay.html'));
+  let selectedLanguage = 'english';
   overlayWindow.webContents.on('did-finish-load', () => {
     overlayWindow.webContents.send('load-overlay-data', selectedConfig);
     overlayWindow.webContents.send('set-language', selectedLanguage);
+  });
+
+  const isReady = [
+    new Promise(function (resolve) {
+      overlayWindow.once('ready-to-show', () => {
+        return resolve();
+      }); //Window is loaded and ready to be drawn
+    }),
+  ];
+  Promise.all(isReady).then(() => {
+    overlayWindow.show();
   });
 
   overlayWindow.on('closed', () => {
     overlayWindow = null;
   });
 }
-
 function createNotificationWindow(message) {
   const display = screen.getPrimaryDisplay();
   const { width, height } = display.workAreaSize;
@@ -139,147 +328,11 @@ function createNotificationWindow(message) {
 try {
   if (app.requestSingleInstanceLock() !== true) app.quit();
 
-  const manifest = require('../package.json');
-
-  if (manifest.config['disable-gpu']) app.disableHardwareAcceleration();
-  if (manifest.config.appid) app.setAppUserModelId(manifest.config.appid);
-
-  let MainWin = null;
-
   app
     .on('ready', function () {
-      let options = manifest.config.window;
-      options.show = false;
-      options.webPreferences = {
-        devTools: manifest.config.debug || false,
-        nodeIntegration: true,
-        contextIsolation: false,
-        webviewTag: false,
-        v8CacheOptions: manifest.config.debug ? 'none' : 'code',
-        enableRemoteModule: true,
-      };
-
-      //electron 9 crash if no icon exists to specified path
-      try {
-        fs.accessSync(options.icon, fs.constants.F_OK);
-      } catch {
-        delete options.icon;
-      }
-
-      MainWin = new BrowserWindow(options);
-
-      //Frameless
-      if (options.frame === false) MainWin.isFrameless = true;
-
-      //Debug tool
-      if (manifest.config.debug) {
-        MainWin.webContents.openDevTools({ mode: 'undocked' });
-        MainWin.isDev = true;
-        console.info((({ node, electron, chrome }) => ({ node, electron, chrome }))(process.versions));
-        try {
-          const contextMenu = require('electron-context-menu')({
-            append: (defaultActions, params, browserWindow) => [
-              {
-                label: 'Reload',
-                visible: params,
-                click: () => {
-                  MainWin.reload();
-                },
-              },
-            ],
-          });
-        } catch (err) {
-          dialog.showMessageBoxSync({
-            type: 'warning',
-            title: 'Context Menu',
-            message: 'Failed to initialize context menu.',
-            detail: `${err}`,
-          });
-        }
-      }
-
-      //User agent
-      MainWin.webContents.userAgent = manifest.config['user-agent'];
-      session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-        details.requestHeaders['User-Agent'] = manifest.config['user-agent'];
-        callback({ cancel: false, requestHeaders: details.requestHeaders });
-      });
-
-      //External open links
-      const openExternal = function (event, url) {
-        if (!url.startsWith('file:///')) {
-          event.preventDefault();
-          shell.openExternal(url).catch(() => {});
-        }
-      };
-      MainWin.webContents.on('will-navigate', openExternal); //a href
-      MainWin.webContents.on('new-window', openExternal); //a href target="_blank"
-
-      //enable ipc
-      ipc.window(MainWin);
-      remote.initialize();
-
-      MainWin.loadFile(manifest.config.window.view);
-
-      const isReady = [
-        new Promise(function (resolve) {
-          MainWin.once('ready-to-show', () => {
-            return resolve();
-          }); //Window is loaded and ready to be drawn
-        }),
-        new Promise(function (resolve) {
-          ipcMain.handleOnce('components-loaded', () => {
-            return resolve();
-          }); //Wait for custom event
-        }),
-      ];
-
-      Promise.all(isReady).then(() => {
-        MainWin.show();
-        MainWin.focus();
-
-        setInterval(() => {
-          const command = `powershell -NoProfile -Command "Get-Process | Where-Object { $_.Path -ne $null } | ForEach-Object { try { $desc = (Get-Item $_.Path).VersionInfo.FileDescription } catch { $desc = 'N/A' }; $memoryUsage = $_.WorkingSet / 1MB; Write-Output ('{0}|{1}|{2}|{3}' -f $_.Name, $_.Id, $desc, $memoryUsage) }"`;
-          let found = false;
-          exec(command, (error, stdout) => {
-            if (!error) {
-              const lines = stdout.trim().split('\r\n');
-              for (const line of lines) {
-                const [name, pid, description, memory] = line.trim().split('|');
-                if (name.toLowerCase() === 'node' && description.toLowerCase().includes('achievement watcher')) {
-                  found = true;
-                }
-                //TODO: check for processes with big ram usage, might be a game so lets try to get exe path and args used and auto add to exeList
-              }
-            }
-            MainWin.webContents.send('watchdog-status', found);
-          });
-        }, 5000);
-
-        //overlay here to debug
-        globalShortcut.register('Control+Shift+O', () => {
-          if (overlayWindow && !overlayWindow.isDestroyed()) {
-            overlayWindow.close();
-            overlayWindow = null;
-          } else {
-            if (!selectedConfig) {
-              if (mainWindow && !mainWindow.isDestroyed()) {
-                mainWindow.webContents.send('notify', {
-                  message: '⚠️ Select first a config!',
-                  color: '#ff9800',
-                });
-              }
-              return;
-            }
-
-            createOverlayWindow(selectedConfig);
-          }
-        });
-      });
-
-      MainWin.on('closed', () => {
-        MainWin = null;
-      });
+      const args = require('minimist')(process.argv.slice(1));
+      let overlayAppid = args['overlay-appid'];
+      overlayAppid ? createOverlayWindow(overlayAppid) : createMainWindow();
     })
     .on('window-all-closed', function () {
       app.quit();
@@ -290,17 +343,9 @@ try {
       });
     })
     .on('second-instance', (event, argv, cwd) => {
-      if (argv.includes('--overlay-appid=')) {
-        //TODO: open overlay for that appid
-        if (overlayWindow) {
-        } else {
-        }
-      } else {
-        if (MainWin) {
-          if (MainWin.isMinimized()) MainWin.restore();
-          MainWin.focus();
-        }
-      }
+      const args = require('minimist')(process.argv.slice(1));
+      let overlayAppid = args['overlay-appid'];
+      overlayAppid ? createOverlayWindow(overlayAppid) : createMainWindow();
     });
 } catch (err) {
   dialog.showErrorBox('Critical Error', `Failed to initialize:\n${err}`);
